112c1ec8f2353ba3025274ea6cb39e77eb03fdb4
Namrata-Ag09 <151063426+Namrata-Ag09@users.noreply.github.com>
2024-06-15 13:38:24 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+ name: "Namrata Agarwal",
+  title: "Technology Enthusiast",
+  email: "namrataagarwal0109@gmail.com",
+  gitHub: "Namrata-Ag09",
+  instagram: "",
+  linkedIn: "Namrata Agarwal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
