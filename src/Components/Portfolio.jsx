8a76d5d41db64bccf4c6614f9b837329dd604dc7
Namrata-Ag09 <151063426+Namrata-Ag09@users.noreply.github.com>
2024-06-15 13:49:22 +0000

    changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "/workspaces/Portfolio/src/images/nathan-da-silva-k-rKfqSm4L4-unsplash.jpg";
+import image from "../images/nathan-da-silva-k-rKfqSm4L4-unsplash.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
