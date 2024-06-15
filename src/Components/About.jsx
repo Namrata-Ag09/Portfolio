77c1e60217bdb3e714222c4c345b941ecd624617
Namrata-Ag09 <151063426+Namrata-Ag09@users.noreply.github.com>
2024-06-15 07:42:33 +0000

    Changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am pursuing my undergraduate degree in Bachelor's of Technology in Computer Science & Engineering with specialisation in Artificial Intelligence & Machine Learning. I enjoy to code and explore new technologies.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Java Programming",
+  "Python Programming",
+  "Django",
+  "Front End Development",
+  "Creative Writing",
+  "Problem Solving",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I have a good command over Java and Python Programming.I am passionate about Artificial Intelligence and Machine Learning and I enjoy learning about intelligent systems using these technologies.";
 
 const About = () => {
   return (
